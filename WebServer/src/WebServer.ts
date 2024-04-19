cf0cf1f64bdea765b701a2ae1e20a4b0573e05fa
zhiyan114 <zhiyan114@zhiyan114.com>
2024-04-18 23:53:24 -0400

    Finish with websocket implementation

--- WebServer/src/WebServer.ts
+++ WebServer/src/WebServer.ts
@@ -1,3 +1,68 @@
 import express from 'express';
+import { createServer } from 'http';
+import websocket from "ws";
+import { URL } from 'url';
+import { redis, redisPrefix } from './database';
+
 
 export const server = express();
+const _server = createServer(server);
+
+
+const mainRoute = server.route('/auth');
+const rediReqPrefix = `${redisPrefix}RFID:REQUEST:`;
+// Verify auth request
+mainRoute.get((req, res)=> {
+
+});
+
+// Submit auth request
+mainRoute.post((req, res)=> {
+
+});
+
+// Extend ws client to include request key
+interface wsClient extends websocket {
+  reqKey: string;
+}
+
+
+// Websocket for real-time updates
+export const clients: wsClient[] = [];
+export function searchClient(key: string) {
+  return clients.find((client)=> client.reqKey === key);
+}
+
+
+export const ws_server = new websocket.Server({server: _server, path: '/auth'});
+ws_server.on('connection', async (ws: wsClient, req)=> {
+  // Sanitation Check
+  if(!req.url)
+    return ws.close(1008, "Invalid Request");
+
+  // Verify if the request exists
+  const reqKey = (new URL(req.url)).searchParams.get('reqKey');
+  if(!reqKey)
+    return ws.close(1008, "Invalid Request");
+  if((await redis.exists(`${rediReqPrefix}${reqKey}`)) === 0)
+    return ws.close(1008, "Invalid Request");
+
+  // Setup the valid client
+  ws.reqKey = reqKey;
+  clients.push(ws);
+  ws.on('close', ()=> {
+    const index = clients.indexOf(ws);
+    if(index !== -1)
+      clients.splice(index, 1);
+  });
+  ws.on('message', async ()=> {
+    ws.close(1008, "Invalid Request");
+  });
+
+});
+
+
+
+_server.listen(80, ()=> {
+  console.log("RFID REST Server is listening on port 80");
+});
--- WebServer/src/index.ts
+++ WebServer/src/index.ts
@@ -1,6 +1,7 @@
 import {Socket, createServer} from "net";
-import {RFIDAuthData} from "./protobuf";
+import {RFIDAuthData} from "./definitions";
 import { redis, redisPrefix } from "./database";
+import { searchClient } from "./WebServer";
 
 
 enum reqType {
@@ -19,22 +20,27 @@ export const server = createServer((socket)=> {
     switch(response.ID) {
     case reqType.checkAuth: {
       const reqKey = `${redisPrefix}RFID:REQUEST:${Buffer.from(response.data).toString('base64')}`;
-      const req = await redis.get(reqKey);
+      const req = (await redis.get(reqKey))?.split(":");
       if(!req) {
         console.log("Received invalid auth request from client");
         socket.write(RFIDAuthData.encode(new RFIDAuthData({ID: reqType.checkAuth, data: Buffer.from('Invalid RFID Card', 'utf-8')})).finish());
         break;
-      } else if(req === "approved") {
+      } else if(req[1] === "approved") {
         console.log("Received Duplicate Auth Request from Client");
         socket.write(RFIDAuthData.encode(new RFIDAuthData({ID: reqType.checkAuth, data: Buffer.from('Authentication already completed, maybe a site glitch?', 'utf-8')})).finish());
         break;
-      } else if(req === "pending") {
+      } else if(req[1] === "pending") {
         // Approve user's request
-        await redis.set(reqKey, "approved");
+        await redis.set(reqKey, `${req[0]}:approved`);
+        socket.write(RFIDAuthData.encode(new RFIDAuthData({ID: reqType.checkAuth, data: Buffer.from('Authentication Success! You should be automatically logged in, if not, you can use the check button to manually do so.', 'utf-8')})).finish());
+        const client = searchClient(reqKey);
+        client?.send("approved");
+        client?.close();
+        break;
       }
       break;
     }
-    
+
     default:
       console.log("Invalid Request Received from Client");
       socket.write(RFIDAuthData.encode(new RFIDAuthData({ID: 0, data: Buffer.from('Invalid Request ID', 'utf-8')})).finish());
